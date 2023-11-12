7c06b534f1995d9ffd6f2690926091674debed6d
anastasiia-natina <132943209+anastasiia-natina@users.noreply.github.com>
2023-11-12 19:12:11 +0100

    creating-timers

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,19 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startButton.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(intervalId);
+  document.body.style.backgroundColor = '';
+});
---
+++ src/js/03-promises.js
@@ -0,0 +1,38 @@
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const firstDelay = parseInt(document.querySelector('[name="delay"]').value);
+  const step = parseInt(document.querySelector('[name="step"]').value);
+  const amount = parseInt(document.querySelector('[name="amount"]').value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, firstDelay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
